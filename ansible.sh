b92ba73b349033ceaa68c00be94303b30cd99bfd
Miguel Andres Isaza Barona <misazab@psl.com.co>
2020-10-09 08:42:13 -0500

    stable version

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-#Script to install ansible on remote machine
+
+# Install ansible
 sudo apt-get update -y
-sudo apt-get install ansible -y
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+sudo apt-get install -y python python-pip
+ansible --version
+
+# Configure environment for ansible local provision
+sudo apt-get install -y git
+pip install pip --upgrade
+pip install ansible --upgrade

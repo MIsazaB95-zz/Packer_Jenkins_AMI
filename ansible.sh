bfc19632ff4fed5a275867a4ca4fe02caeeb1f44
MIsazaB95 <miguel.isazab@perficient.com>
2020-10-15 07:24:57 -0700

    Stable version 1.1

--- ansible.sh
+++ ansible.sh
@@ -4,13 +4,10 @@
 sudo apt-get update -y
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible
-sudo apt-get install -y python python-pip
+sudo apt-get install -y ansible curl zip git maven
 ansible --version
 
-# Configure environment for ansible local provision
-sudo apt-get install -y git
-pip install pip --upgrade
-pip install ansible --upgrade
-
 # Install aws cli v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
